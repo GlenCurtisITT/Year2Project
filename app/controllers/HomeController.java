59e9220245e282c61fc304b81a29e6d0bd137b8a
Enda Lee <enda.lee@ittdublin.ie>
2017-03-13 11:38:43 +0000

    Added in view profile to search page. Make appointment button and add consultant button added with no functionality.

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -57,6 +57,16 @@ public class HomeController extends Controller {
         return ok(searchPatient.render(patientList, getUserFromSession()));
     }
 
+    public Result viewPatient(){
+        Patient p = new Patient();
+        return ok(viewPatient.render(getUserFromSession(), p));
+    }
+
+    public Result viewPatientByID(String mrn){
+        Patient p = Patient.find.byId(mrn);
+        return ok(viewPatient.render(getUserFromSession(), p));
+    }
+
     public Result addUserSubmit(){
         DynamicForm newUserForm = formFactory.form().bindFromRequest();
         Form errorForm = formFactory.form().bindFromRequest();
