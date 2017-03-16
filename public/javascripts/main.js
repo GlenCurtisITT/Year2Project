2e3fc162af845db3fc3ac1bdc31633c3553ef9e3
Glen Connolly <connollyglen93@gmail.com>
2017-03-16 15:32:16 +0000

    Merge branch 'AppointmentClass' of https://github.com/GlenCurtisITT/Year2Project into AppointmentClass
    # Conflicts:
    #	data/hpmsDB.mv.db
    
    serializing patient objects on delete and compressing (may need to remove compressions if issues are faced during testing)
    
    added extra error checking with appointments and changed viewPatient to show all appointments booked for that patient
    
    Included rescheduling of appointments, but need to hide the form for inputting date and need to structure code better in reschedule appointments so as not to save to the DB implicitly while in HomeController

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,3 +9,15 @@ setTimeout(fade_out, 3000);
 function fade_out() {
     $("#fadeOut").fadeOut().empty();
 }
+
+function unhide(clickedButton, divID) {
+    var item = document.getElementById(divID);
+    if (item) {
+        if(item.className=='hidden'){
+            item.className = 'unhidden' ;
+            clickedButton.value = 'Cancel'
+        }else{
+            item.className = 'hidden';
+            clickedButton.value = 'Reschedule Appointment'
+        }
+    }}
